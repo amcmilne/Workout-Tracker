b68116a571dfebcbc3a3e8e3a04c0e4ffb1ab519
amcmilne <amcmilne@gmail.com>
2021-02-22 17:38:57 -0500

    deploy

--- server.js
+++ server.js
@@ -1,39 +1,70 @@
-const express = require("express");
-const logger = require("morgan");
-//const mongoose = require("mongoose");
-const connectDB = require ("./DB/connection2");
-require('dotenv').config();
+// const express = require("express");
+// const logger = require("morgan");
+// //const mongoose = require("mongoose");
+// const connectDB = require ("./DB/connection2");
+// require('dotenv').config();
 
-connectDB();
-// Sets up the Express App
-// =============================================================
-const app = express();
-const PORT = process.env.PORT || 3000;
+// connectDB();
+// // Sets up the Express App
+// // =============================================================
+// const app = express();
+// const PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
-//===============================================================
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// // Sets up the Express app to handle data parsing
+// //===============================================================
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-app.use(express.static("public"));
-app.use(logger("dev"));
+// app.use(express.static("public"));
+// app.use(logger("dev"));
 
-// // Sets up the connection
-// // ==============================================================
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
+// // // Sets up the connection
+// // // ==============================================================
+// // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+// //   useNewUrlParser: true,
+// //   useUnifiedTopology: true,
+// //   useCreateIndex: true,
+// //   useFindAndModify: false,
+// // });
+
+// // Sets up required routes
+// // =============================================================
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
+
+// // Starts the server to begin listening
+// // =============================================================
+// app.listen(PORT, () => {
+//   console.log(`App running on port ${PORT}!`);
 // });
 
-// Sets up required routes
-// =============================================================
+
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-// Starts the server to begin listening
-// =============================================================
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
