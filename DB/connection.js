7d59d14f258cb017e41b31e06bbfb849e037c84b
amcmilne <amcmilne@gmail.com>
2021-02-22 10:38:21 -0500

    another fix

--- DB/connection.js
+++ DB/connection.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
-const MONGODB_URI = "mongodb+srv://dbUser:dbUser@cluster0.nczo0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const MONGODB_URI =
+  "mongodb+srv://dbUser:dbUser@cluster0.nczo0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const connectDB = async () => {
-   await mongoose.connect(MONGODB_URI, {
-      useUnifiedTopology: true, 
-      useNewUrlParser: true
-   });
-   console.log("db has connected");
-}
+  await mongoose.connect(MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
+  console.log("db has connected");
+};
 
 module.exports = connectDB;
